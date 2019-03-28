97a14595a5c3591bca793cd7d9fa7664e532ca45
Anna Zhukova <zhutchok@gmail.com>
2019-03-28 11:52:10 +0100

    pipelines for matadata extraction, alignment, tree reconstruction, and acr

--- py/location_enchancer.py
+++ py/location_enchancer.py
@@ -3,16 +3,16 @@ from hdx.location.country import Country
 
 
 def add_loc_info(df):
-    df.loc[df['city'].str.contains('Puerto Rico') == True, 'country'] = 'Puerto Rico'
+    df.loc[df['location_details'].str.contains('Puerto Rico') == True, 'country'] = 'Puerto Rico'
     countries = df['country'].unique()
 
     def get_iso3(country):
-        if not country:
+        if pd.isna(country):
             return None
         return Country.get_iso3_country_code_fuzzy(country)[0]
 
     country2iso3 = {_: get_iso3(_) for _ in countries}
-    iso32info = {_: Country.get_country_info_from_iso3(_) for _ in country2iso3.values()}
+    iso32info = {_: Country.get_country_info_from_iso3(_) for _ in country2iso3.values() if _}
 
     def get_Location(iso3):
         if not iso3:
@@ -36,8 +36,17 @@ def add_loc_info(df):
                                                  if iso32info[_]['Intermediate Region Name']
                                                  else iso32info[_]['Sub-region Name']) if _ in iso32info else None)
     df['location'] = df['country_code'].apply(get_Location)
-    df.loc[df['city'].str.contains('Hawaii') == True, 'location'] = 'Oceania'
+    df.loc[df['location_details'].str.contains('Hawaii') == True, 'location'] = 'Oceania'
     return df
+
+
+def generalise_host(df):
+    df['host_type'] = 'monkey'
+    df.loc[pd.isna(df['host']), 'host_type'] = None
+    df.loc[df['host'].str.contains('Aedes') == True, 'host_type'] = 'mosquito'
+    df.loc[df['host'].str.contains('mosquito') == True, 'host_type'] = 'mosquito'
+    df.loc[df['host'].str.contains('Armigeres') == True, 'host_type'] = 'mosquito'
+    df.loc[df['host'] == 'Homo sapiens', 'host_type'] = 'human'
 
 
 if '__main__' == __name__:
@@ -50,4 +59,6 @@ if '__main__' == __name__:
     params = parser.parse_args()
 
     df = pd.read_csv(params.input_data, sep='\t', header=0, index_col=0)
-    add_loc_info(df).to_csv(params.output_data, sep='\t', index_label='accession')
+    add_loc_info(df)
+    generalise_host(df)
+    df.to_csv(params.output_data, sep='\t', index_label='accession')
--- py/pretempest.py
+++ py/pretempest.py
@@ -2,11 +2,44 @@ import pandas as pd
 from ete3 import Tree
 
 
-def date2years(d, default=''):
+def date2years(d, default=None):
     if pd.notnull(d):
-        return d.year
+        first_jan_this_year = pd.datetime(year=d.year, month=1, day=1)
+        day_of_this_year = d - first_jan_this_year
+        first_jan_next_year = pd.datetime(year=d.year + 1, month=1, day=1)
+        days_in_this_year = first_jan_next_year - first_jan_this_year
+        return d.year + day_of_this_year / days_in_this_year
     else:
         return default
+
+
+def remove_certain_leaves(tr, to_remove=lambda node: False):
+    """
+    Removes all the branches leading to leaves identified positively by to_remove function.
+    :param tr: the tree of interest (ete3 Tree)
+    :param to_remove: a method to check is a leaf should be removed.
+    :return: void, modifies the initial tree.
+    """
+
+    tips = [tip for tip in tr if to_remove(tip)]
+    for node in tips:
+        if node.is_root():
+            return None
+        parent = node.up
+        parent.remove_child(node)
+        # If the parent node has only one child now, merge them.
+        if len(parent.children) == 1:
+            brother = parent.children[0]
+            brother.dist += parent.dist
+            if parent.is_root():
+                brother.up = None
+                tr = brother
+            else:
+                grandparent = parent.up
+                grandparent.remove_child(parent)
+                grandparent.add_child(brother)
+    tr.dist = 0
+    return tr
 
 
 if '__main__' == __name__:
@@ -18,10 +51,9 @@ if '__main__' == __name__:
     parser.add_argument('--input_tree', required=True, type=str)
     parser.add_argument('--output_tree', required=True, type=str)
     parser.add_argument('--date_col', required=True, type=str)
-    parser.add_argument('--genotype_col', required=True, type=str)
     params = parser.parse_args()
 
-    df = pd.read_csv(params.data, index_col=0, sep='\t')[[params.date_col, params.genotype_col]]
+    df = pd.read_csv(params.data, index_col=0, sep='\t')[[params.date_col]]
     try:
         df[params.date_col] = pd.to_datetime(df[params.date_col], format='%Y')
     except ValueError:
@@ -36,9 +68,10 @@ if '__main__' == __name__:
     df[params.date_col] = df[params.date_col].apply(lambda d: date2years(d))
     df.index = df.index.map(str)
 
-    tree = Tree(params.input_tree)
+    print(df.head())
+
+    tree = remove_certain_leaves(Tree(params.input_tree), lambda _: pd.isna(df.loc[_.name, params.date_col]))
     for _ in tree:
-        _.name = '{}_{}_{}'.format(_.name, str(df.loc[_.name, params.genotype_col]).split(' ')[0], 
-                                   df.loc[_.name, params.date_col])
+        _.name = '{}_{}'.format(_.name, df.loc[_.name, params.date_col])
     tree.write(outfile=params.output_tree)
 
